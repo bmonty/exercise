59f8bd4a469a2da68a087a6fe60ffce5002d73da
Benjamin Montgomery <bmontgom@montynet.org>
2020-07-12 14:53:46 -0400

    style table

--- src/components/Senators.js
+++ src/components/Senators.js
@@ -6,17 +6,27 @@ const Senators = (props) => {
 
     return (
         <div>
-            <center><h1>Current Senators</h1></center>
-            <table>
+            <h1 className="text-center">Current U.S. Senators</h1>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col"> </th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Party</th>
+                        <th scope="col">State</th>
+                    </tr>
+                </thead>
                 <tbody>
                 {senators.objects.map((senator) => (
                     <tr key={senator.person.bioguideid}>
-                        <td><img src={(() => {
-                            const urlArray = senator.person.link.split('/');
-                            const id = urlArray[urlArray.length - 1];
-                            return `https://www.govtrack.us/static/legislator-photos/${id}-100px.jpeg`;
-                        })()} alt={'Senator ' + senator.person.lastname}></img></td>
-                        <td><h5>{senator.person.firstname} {senator.person.lastname}</h5></td>
+                        <td>
+                            <img className="img-thumbnail" src={(() => {
+                                const urlArray = senator.person.link.split('/');
+                                const id = urlArray[urlArray.length - 1];
+                                return `https://www.govtrack.us/static/legislator-photos/${id}-100px.jpeg`;
+                            })()} alt={'Senator ' + senator.person.lastname}></img>
+                        </td>
+                        <td><p className="lead">{senator.person.firstname} {senator.person.lastname}</p></td>
                         <td>{senator.party}</td>
                         <td>{senator.state}</td>
                     </tr>
