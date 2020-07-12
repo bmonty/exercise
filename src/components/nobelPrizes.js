f259fab669f590d17647e02002fcd750c420dc1d
Benjamin Montgomery <bmontgom@montynet.org>
2020-07-12 16:51:09 -0400

    add pagination (#6)

--- src/components/nobelPrizes.js
+++ src/components/nobelPrizes.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LaureateTable from './laureateDisplay';
+import Pagination from './Pagination';
 
 const NobelPrizes = (props) => {
+    const [nobelPrizesCurrentPage, setNobelPrizesCurrentPage] = useState(1);
+    const [nobelPrizePageSize, setNobelPrizePageSize] = useState(5);
+
     const { nobelPrizes } = props;
     if (!nobelPrizes) return <p>Something went wrong...</p>;
+
+    const indexLast = nobelPrizesCurrentPage * nobelPrizePageSize;
+    const indexFirst = indexLast - nobelPrizePageSize;
+    const current = nobelPrizes.prizes.slice(indexFirst, indexLast);
 
     return (
         <div>
             <h1 className="text-center">Nobel Prizes</h1>
-            {nobelPrizes.prizes.map((prize) => (
+            <Pagination currentPage={nobelPrizesCurrentPage} itemsPerPage={nobelPrizePageSize} totalItems={nobelPrizes.prizes.length} paginate={setNobelPrizesCurrentPage} />
+            {current.map((prize) => (
                 <div key={prize.year + prize.category}>
                     <h2 className="text-center">{prize.year} - {prize.category}</h2>
                     <LaureateTable laureates={prize.laureates} />
