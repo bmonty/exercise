fe37c3bcfafda604bc20ca7a0bf6552686bfbf44
Benjamin Montgomery <bmontgom@montynet.org>
2020-07-12 13:33:31 -0400

    add key value for table rows

--- src/components/Senators.js
+++ src/components/Senators.js
@@ -2,25 +2,26 @@ import React from 'react';
 
 const Senators = (props) => {
     const { senators } = props;
-    if (!senators) return <p>There are no senators, something went wrong.</p>;
-    console.log(senators)
+    if (!senators) return <p>Something went wrong...</p>;
 
     return (
         <div>
             <center><h1>Current Senators</h1></center>
             <table>
-            {senators.objects.map((senator) => (
-                <tr>
-                    <td><img src={(() => {
-                        const urlArray = senator.person.link.split('/');
-                        const id = urlArray[urlArray.length - 1];
-                        return `https://www.govtrack.us/static/legislator-photos/${id}-100px.jpeg`;
-                    })()} alt={'Senator ' + senator.person.lastname}></img></td>
-                    <td><h5>{senator.person.firstname} {senator.person.lastname}</h5></td>
-                    <td>{senator.party}</td>
-                    <td>{senator.state}</td>
-                </tr>
-            ))}
+                <tbody>
+                {senators.objects.map((senator) => (
+                    <tr key={senator.person.bioguideid}>
+                        <td><img src={(() => {
+                            const urlArray = senator.person.link.split('/');
+                            const id = urlArray[urlArray.length - 1];
+                            return `https://www.govtrack.us/static/legislator-photos/${id}-100px.jpeg`;
+                        })()} alt={'Senator ' + senator.person.lastname}></img></td>
+                        <td><h5>{senator.person.firstname} {senator.person.lastname}</h5></td>
+                        <td>{senator.party}</td>
+                        <td>{senator.state}</td>
+                    </tr>
+                ))}
+                </tbody>
             </table>
         </div>
     )
