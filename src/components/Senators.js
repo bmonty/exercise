23998080705362aeb8e8512b955eb2f1ef479591
Benjamin Montgomery <bmontgom@montynet.org>
2020-07-12 12:16:31 -0400

    switch to table view and add picture of senator

--- src/components/Senators.js
+++ src/components/Senators.js
@@ -4,20 +4,24 @@ const Senators = (props) => {
     const { senators } = props;
     if (!senators) return <p>There are no senators, something went wrong.</p>;
     console.log(senators)
+
     return (
         <div>
             <center><h1>Current Senators</h1></center>
+            <table>
             {senators.objects.map((senator) => (
-                <div>
-                    <table>
-                        <tr>
-                            <td><h5>{senator.person.firstname} {senator.person.lastname}</h5></td>
-                            <td>{senator.party}</td>
-                            <td>{senator.state}</td>
-                        </tr>
-                    </table>
-                </div>
+                <tr>
+                    <td><img src={(() => {
+                        const urlArray = senator.person.link.split('/');
+                        const id = urlArray[urlArray.length - 1];
+                        return `https://www.govtrack.us/static/legislator-photos/${id}-100px.jpeg`;
+                    })()} alt={'Senator ' + senator.person.lastname}></img></td>
+                    <td><h5>{senator.person.firstname} {senator.person.lastname}</h5></td>
+                    <td>{senator.party}</td>
+                    <td>{senator.state}</td>
+                </tr>
             ))}
+            </table>
         </div>
     )
 };
